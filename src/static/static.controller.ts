4e4cb9cb788d63eb0b4203759d18f3856137be6b
hassangomaa <animehassan05@gmail.com>
2022-10-24 16:39:21 +0200

    all statics Done!

--- src/static/static.controller.ts
+++ src/static/static.controller.ts
@@ -18,5 +18,20 @@ export class StaticController {
   @Get('/homepage/second-page')
   secondPage() {
     return this.staticService.secondSection();
+  }  
+  
+  @Get('homepage/first-section')
+  firstPage() {
+    return this.staticService.firstSection();
   }
+  @Get('homepage/footer')
+  footerSection() {
+    return this.staticService.footerSection();
+  }
+  @Get('homepage/pet-needs')
+  petNeeds() {
+    return this.staticService.petNeedsSection();
+  }
+
+
 }
