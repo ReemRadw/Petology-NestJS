7539d780dddc3cde0f1c26aa99d0fe4e14d28b7e
hassangomaa <animehassan05@gmail.com>
2022-10-26 09:52:54 +0200

    google 1.1

--- src/strategy/google.strategy.ts
+++ src/strategy/google.strategy.ts
@@ -1,35 +1,42 @@
-
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import passport from 'passport';
-import {Strategy} from 'passport-token-google';
+import { Strategy } from 'passport-token-google';
 import { PrismaService } from 'src/prisma.service';
 
- 
- @Injectable()
-export class GoogleStrategy extends PassportStrategy(Strategy, 'google'){
-    constructor(private prisma: PrismaService) {
-        super()
-    }
-    async validate(payload: { sub: number; email: string }) {
-        const user = await this.prisma.user.findUnique({
-          where: {
-            id: payload.sub,
-          },
-        });
-        delete user.password;
-        return user;
-      }
-  
-//     passport.use(new Strategy({
-//         clientID: process.env.GOOGLE_CLIENT_ID,
-//         clientSecret: process.env.GOOGLE_CLIENT_SECRET
-//     },
-//   function(idToken, refreshToken, profile, done) {
-//     fetchGoogleUser(profile, idToken)
-//       .then((user) => done(null, user))
-//       .catch((err) => {
-//         // Handle the error
-//         done(null, {})
-//       });
+//please do'nt respond
+
+@Injectable()
+export class GoogleStrategy extends PassportStrategy(
+  Strategy,
+  'google',
+) {
+  constructor(private prisma: PrismaService) {
+    super();
   }
+  async validate(payload: {
+    sub: number;
+    email: string;
+  }) {
+    const user =
+      await this.prisma.user.findUnique({
+        where: {
+          id: payload.sub,
+        },
+      });
+    delete user.password;
+    return user;
+  }
+
+  //     passport.use(new Strategy({
+  //         clientID: process.env.GOOGLE_CLIENT_ID,
+  //         clientSecret: process.env.GOOGLE_CLIENT_SECRET
+  //     },
+  //   function(idToken, refreshToken, profile, done) {
+  //     fetchGoogleUser(profile, idToken)
+  //       .then((user) => done(null, user))
+  //       .catch((err) => {
+  //         // Handle the error
+  //         done(null, {})
+  //       });
+}
